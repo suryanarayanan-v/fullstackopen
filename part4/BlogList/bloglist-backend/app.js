d96e6d0c084e07e079c777c43b3628bd0f1617b3
suryanarayanan-v <suryan.venkat@gmail.com>
2024-08-04 17:05:30 +0530

    Bloglist step 2 complete

---
+++ part4/BlogList/bloglist-backend/app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const config = require('./utils/config')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const Blogs = require('./controllers/blogs')
+
+
+const mongoUrl = config.MONGO_URI
+mongoose.connect(mongoUrl)
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', Blogs)
+
+module.exports = app
---
+++ part4/BlogList/bloglist-backend/utils/config.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+
+const MONGO_URI = process.env.MONGODB_URI;
+
+const PORT = process.env.PORT
+
+module.exports = {
+  MONGO_URI,
+  PORT
+}
